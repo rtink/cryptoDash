cde64bae5854b6b6d47c764effe79b7fff2649c4
Rhonda <rtinkham0408@gmail.com>
2018-12-16 17:41:05 -0500

    Add AppBar

---
+++ src/App/AppBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Bar = styled.div`
+  display: grid;
+  grid-template-columns: 180px auto 150px 100px;
+`
+
+const AppBar = () => {
+  return <Bar>
+      <div>CryptoDash</div>
+      <div></div>
+      <div>Dashboard</div>
+      <div>Settings</div>
+    </Bar>
+}
+
+export default AppBar;
