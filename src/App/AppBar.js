bb674e44b5186ecec5d1afb65a944c64dea718d7
Rhonda <rtinkham0408@gmail.com>
2018-12-16 19:28:03 -0500

    App AppProvider and hook up to AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import {AppContext} from './AppProvider';
 
 const Logo = styled.div`
   font-size: 1.5em;
@@ -18,15 +19,22 @@ const toProperCase = (lower) => {
 const ControlButtonElem = styled.div`
   cursor: pointer;
   ${props => props.active && css`
-    text-shadow: 0px 10px 30px #9f7;
+    text-shadow: 0px 0px 45px #07f707;
   `}
 `
 
-const ControlButton = ({ name, active }) => {
+const ControlButton = ({ name }) => {
   return (
-    <ControlButtonElem active={active}>
-      {toProperCase(name)}
-    </ControlButtonElem>
+    <AppContext.Consumer>
+      {({page, setPage}) => (
+      <ControlButtonElem
+        active={page === name}
+        onClick={() => setPage(name)}
+      >
+        {toProperCase(name)}
+      </ControlButtonElem>
+      )}
+    </AppContext.Consumer>
   )
 }
 
